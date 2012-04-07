24ba0578eef392c701ec82a180f2a2186d160405
Hideto Yoshii <blue_snow_sea_world@yahoo.co.jp>
2012-04-08 02:14:29 +0900

    idente start!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Idente::Application.config.secret_token = '2022e27c69110f6e526e7b9ffa91681c434758d685795b9c8c1f9b325cbb659e65522e95536d7ba677b150fc760ba870e64001a7664688ab71ef187a34300c56'
